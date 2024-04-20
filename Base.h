6827c7c395ba08ccf9cbd9b08412df4873758471
barada02 <chandanbarada2@gmail.com>
2024-04-20 10:25:26 +0530

    changes in appearince
    just changed the font size and Heading

--- Base.h
+++ Base.h
@@ -51,6 +51,13 @@ namespace scan {
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::ListBox^ Shorted_listBox;
+	private: System::Windows::Forms::Panel^ panel4;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::Label^ label4;
+
 
 
 	protected:
@@ -68,7 +75,16 @@ namespace scan {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Base::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel4 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->Shorted_listBox = (gcnew System::Windows::Forms::ListBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->direction_comboBox = (gcnew System::Windows::Forms::ComboBox());
 			this->Disk_Cylinder_textBox = (gcnew System::Windows::Forms::TextBox());
 			this->head_position_textBox = (gcnew System::Windows::Forms::TextBox());
@@ -78,16 +94,22 @@ namespace scan {
 			this->Add_Request_button = (gcnew System::Windows::Forms::Button());
 			this->Reqest_label = (gcnew System::Windows::Forms::Label());
 			this->Request_textBox = (gcnew System::Windows::Forms::TextBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
 			this->panel1->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Controls->Add(this->pictureBox2);
+			this->panel1->Controls->Add(this->panel4);
+			this->panel1->Controls->Add(this->panel2);
+			this->panel1->Controls->Add(this->pictureBox1);
+			this->panel1->Controls->Add(this->Shorted_listBox);
 			this->panel1->Controls->Add(this->label3);
 			this->panel1->Controls->Add(this->label2);
 			this->panel1->Controls->Add(this->label1);
@@ -106,30 +128,115 @@ namespace scan {
 			this->panel1->Size = System::Drawing::Size(1676, 550);
 			this->panel1->TabIndex = 0;
 			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(510, 170);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(85, 70);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 16;
+			this->pictureBox2->TabStop = false;
+			this->pictureBox2->Click += gcnew System::EventHandler(this, &Base::pictureBox2_Click);
+			// 
+			// panel4
+			// 
+			this->panel4->BackColor = System::Drawing::Color::Teal;
+			this->panel4->Location = System::Drawing::Point(743, 227);
+			this->panel4->Name = L"panel4";
+			this->panel4->Size = System::Drawing::Size(19, 321);
+			this->panel4->TabIndex = 15;
+			// 
+			// panel2
+			// 
+			this->panel2->BackColor = System::Drawing::Color::Teal;
+			this->panel2->Location = System::Drawing::Point(743, 80);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(19, 92);
+			this->panel2->TabIndex = 14;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(711, 159);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(85, 80);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 13;
+			this->pictureBox1->TabStop = false;
+			// 
+			// Shorted_listBox
+			// 
+			this->Shorted_listBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Shorted_listBox->FormattingEnabled = true;
+			this->Shorted_listBox->ItemHeight = 27;
+			this->Shorted_listBox->Location = System::Drawing::Point(601, 80);
+			this->Shorted_listBox->Name = L"Shorted_listBox";
+			this->Shorted_listBox->Size = System::Drawing::Size(104, 247);
+			this->Shorted_listBox->TabIndex = 12;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(817, 216);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(85, 23);
+			this->label3->TabIndex = 11;
+			this->label3->Text = L"Direction";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(817, 148);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(155, 23);
+			this->label2->TabIndex = 10;
+			this->label2->Text = L"Disk Cylinder Size";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(817, 93);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(168, 23);
+			this->label1->TabIndex = 9;
+			this->label1->Text = L"Head\\Start Position";
+			// 
 			// direction_comboBox
 			// 
+			this->direction_comboBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->direction_comboBox->FormattingEnabled = true;
 			this->direction_comboBox->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Right", L"Left" });
 			this->direction_comboBox->Location = System::Drawing::Point(1004, 216);
 			this->direction_comboBox->Name = L"direction_comboBox";
-			this->direction_comboBox->Size = System::Drawing::Size(121, 24);
+			this->direction_comboBox->Size = System::Drawing::Size(121, 35);
 			this->direction_comboBox->TabIndex = 8;
 			// 
 			// Disk_Cylinder_textBox
 			// 
+			this->Disk_Cylinder_textBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->Disk_Cylinder_textBox->Location = System::Drawing::Point(1004, 150);
 			this->Disk_Cylinder_textBox->Name = L"Disk_Cylinder_textBox";
-			this->Disk_Cylinder_textBox->Size = System::Drawing::Size(140, 22);
+			this->Disk_Cylinder_textBox->Size = System::Drawing::Size(140, 33);
 			this->Disk_Cylinder_textBox->TabIndex = 7;
-			this->Disk_Cylinder_textBox->Text = L"Disk Cylinder Size";
 			// 
 			// head_position_textBox
 			// 
+			this->head_position_textBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->head_position_textBox->Location = System::Drawing::Point(1004, 95);
 			this->head_position_textBox->Name = L"head_position_textBox";
-			this->head_position_textBox->Size = System::Drawing::Size(100, 22);
+			this->head_position_textBox->Size = System::Drawing::Size(100, 33);
 			this->head_position_textBox->TabIndex = 6;
-			this->head_position_textBox->Text = L"Head Position";
 			// 
 			// SCAN_button
 			// 
@@ -146,20 +253,24 @@ namespace scan {
 			// 
 			// Serve_sequence_listBox
 			// 
+			this->Serve_sequence_listBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->Serve_sequence_listBox->FormattingEnabled = true;
-			this->Serve_sequence_listBox->ItemHeight = 16;
+			this->Serve_sequence_listBox->ItemHeight = 27;
 			this->Serve_sequence_listBox->Location = System::Drawing::Point(1377, 47);
 			this->Serve_sequence_listBox->Name = L"Serve_sequence_listBox";
-			this->Serve_sequence_listBox->Size = System::Drawing::Size(81, 308);
+			this->Serve_sequence_listBox->Size = System::Drawing::Size(81, 301);
 			this->Serve_sequence_listBox->TabIndex = 4;
 			// 
 			// Request_listBox
 			// 
+			this->Request_listBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->Request_listBox->FormattingEnabled = true;
-			this->Request_listBox->ItemHeight = 16;
-			this->Request_listBox->Location = System::Drawing::Point(440, 69);
+			this->Request_listBox->ItemHeight = 27;
+			this->Request_listBox->Location = System::Drawing::Point(401, 80);
 			this->Request_listBox->Name = L"Request_listBox";
-			this->Request_listBox->Size = System::Drawing::Size(104, 260);
+			this->Request_listBox->Size = System::Drawing::Size(104, 247);
 			this->Request_listBox->TabIndex = 3;
 			// 
 			// Add_Request_button
@@ -167,7 +278,7 @@ namespace scan {
 			this->Add_Request_button->BackColor = System::Drawing::SystemColors::ButtonHighlight;
 			this->Add_Request_button->Font = (gcnew System::Drawing::Font(L"Microsoft Tai Le", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->Add_Request_button->Location = System::Drawing::Point(295, 176);
+			this->Add_Request_button->Location = System::Drawing::Point(283, 170);
 			this->Add_Request_button->Name = L"Add_Request_button";
 			this->Add_Request_button->Size = System::Drawing::Size(88, 49);
 			this->Add_Request_button->TabIndex = 2;
@@ -180,7 +291,7 @@ namespace scan {
 			this->Reqest_label->AutoSize = true;
 			this->Reqest_label->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->Reqest_label->Location = System::Drawing::Point(69, 93);
+			this->Reqest_label->Location = System::Drawing::Point(10, 93);
 			this->Reqest_label->Name = L"Reqest_label";
 			this->Reqest_label->Size = System::Drawing::Size(208, 23);
 			this->Reqest_label->TabIndex = 1;
@@ -188,43 +299,23 @@ namespace scan {
 			// 
 			// Request_textBox
 			// 
-			this->Request_textBox->Location = System::Drawing::Point(304, 95);
+			this->Request_textBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Request_textBox->Location = System::Drawing::Point(244, 95);
 			this->Request_textBox->Name = L"Request_textBox";
-			this->Request_textBox->Size = System::Drawing::Size(100, 22);
+			this->Request_textBox->Size = System::Drawing::Size(127, 33);
 			this->Request_textBox->TabIndex = 0;
 			// 
-			// label1
+			// label4
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 13.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(817, 93);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(168, 23);
-			this->label1->TabIndex = 9;
-			this->label1->Text = L"Head\\Start Position";
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(817, 148);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(155, 23);
-			this->label2->TabIndex = 10;
-			this->label2->Text = L"Disk Cylinder Size";
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(817, 216);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(85, 23);
-			this->label3->TabIndex = 11;
-			this->label3->Text = L"Direction";
+			this->label4->Location = System::Drawing::Point(583, 22);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(369, 31);
+			this->label4->TabIndex = 17;
+			this->label4->Text = L"SCAN Disc Scheduling Algorithm";
 			// 
 			// Base
 			// 
@@ -237,6 +328,8 @@ namespace scan {
 			this->Text = L"Base";
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 
 		}
@@ -301,7 +394,7 @@ namespace scan {
 			direction = !direction;
 
 			// Reset currentIndex to headIndex
-			currentIndex = direction?headIndex+1 : headIndex-1;
+			currentIndex = direction?headIndex : headIndex-1;
 
 			// Continue moving head in the opposite direction, servicing requests
 			while (currentIndex >= 0 && currentIndex < sortedRequests->Count) {
@@ -352,6 +445,16 @@ private: System::Void SCAN_button_Click(System::Object^ sender, System::EventArg
 
 	PopulateServeSequenceListBox();
 
+}
+private: System::Void pictureBox2_Click(System::Object^ sender, System::EventArgs^ e) {
+	List<int>^ sortedRequests = gcnew List<int>(requests);
+	sortedRequests->Sort();
+	Shorted_listBox->Items->Clear(); // Clear the existing items
+
+	// Loop through the requests list and add each request to the Request_listBox
+	for each (int shorted in sortedRequests) {
+		Shorted_listBox->Items->Add(shorted);
+	}
 }
 };
 }
